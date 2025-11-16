423789d9f41768ae31e34bf75b26176da8c94136
Shaikh Sharukh Mohd Shakil <sshaikh@descartes.com>
2025-11-16 17:28:12 +0530

    updated the s3 backend file

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-s3-backend-tws-hackathon"
+    bucket = "terraform-s3-backend-tws-hackathon-2027"
     key    = "backend-locking"
     region = "eu-west-1"
     use_lockfile = true
